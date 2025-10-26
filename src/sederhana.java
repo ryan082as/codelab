0ea834796f769395e3b31d9d6fe9ba6facd54608
Ryan a.s <bjtbro47@gmail.com>
2025-10-26 12:12:30 +0700

    mk1

---
+++ src/sederhana.java
@@ -0,0 +1,7 @@
+class sederhana {
+    static void main() {    //psvm >> enter
+        for (int i = 0; i < 5; i++){
+            System.out.println("iterasi ke-" + i + ": " + (i+1));   // sout >> enter
+        }
+    }
+}
